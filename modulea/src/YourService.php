3a680971640a93a4ca495e8dbad2532935ace833
Louis AUTHIE <65625876+LouisAUTHIE@users.noreply.github.com>
2023-12-16 20:14:32 +0100

    Update YourService.php

--- modulea/src/YourService.php
+++ modulea/src/YourService.php
@@ -1,14 +1,11 @@
 <?php
 namespace PrestaShop\Module\Modulea;
 
-class YourService
+abstract class YourService
 {
     public function __construct()
     {
     }
 
-    public function sayHello()
-    {
-        dd('Hello from modulea');
-    }
+    abstract public function sayHello();
 }
